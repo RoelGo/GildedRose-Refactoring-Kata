f8d38dda3c837cba1cd5e57a09e23d1ed84a5e04
roelg <roel@wheelhouse.be>
2023-06-23 16:02:16 +0200

    refactor: extract some magic values

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,3 +1,4 @@
+// Important! Do not touch. Goblin wil get angry!
 export class Item {
   name: string;
   sellIn: number;
@@ -10,6 +11,12 @@ export class Item {
   }
 }
 
+const AGED_BRIE = 'Aged Brie';
+const BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+const MAX_QUALITY = 50;
+const MIN_QUALITY = 0;
+
 export class GildedRose {
   items: Array<Item>;
 
@@ -19,37 +26,37 @@ export class GildedRose {
 
   updateQuality() {
     this.items.forEach(item => {
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+      if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES) {
+        if (item.quality > MIN_QUALITY) {
+          if (item.name != SULFURAS) {
             item.quality = item.quality - 1
           }
         }
       } else {
-        if (item.quality < 50) {
+        if (item.quality < MAX_QUALITY) {
           item.quality = item.quality + 1
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (item.name == BACKSTAGE_PASSES) {
             if (item.sellIn < 11) {
-              if (item.quality < 50) {
+              if (item.quality < MAX_QUALITY) {
                 item.quality = item.quality + 1
               }
             }
             if (item.sellIn < 6) {
-              if (item.quality < 50) {
+              if (item.quality < MAX_QUALITY) {
                 item.quality = item.quality + 1
               }
             }
           }
         }
       }
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+      if (item.name != SULFURAS) {
         item.sellIn = item.sellIn - 1;
       }
       if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        if (item.name != AGED_BRIE) {
+          if (item.name != BACKSTAGE_PASSES) {
+            if (item.quality > MIN_QUALITY) {
+              if (item.name != SULFURAS) {
                 item.quality = item.quality - 1
               }
             }
@@ -57,7 +64,7 @@ export class GildedRose {
             item.quality = item.quality - item.quality
           }
         } else {
-          if (item.quality < 50) {
+          if (item.quality < MAX_QUALITY) {
             item.quality = item.quality + 1
           }
         }
